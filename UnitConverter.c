89b8ff94913cbf3415824d3d6d664f9de502518c
Md Tausif Hussain <mdtausifhussain.dev@gmail.com>
2025-06-14 15:03:49 +0530

    Update UnitConverter.c

--- UnitConverter.c
+++ UnitConverter.c
@@ -5,20 +5,20 @@ int main() {
   int tempChoice;
   int currencyChoice;
   int massChoice;
-  int userinputF; // User inputted Fahreinheit;
-  int userinputC; // User inputted Celsius;
-  int userinputUSDtoEuro; // User inputted for USD to EURO;
-  int userinputUSDtoJPY; // User inputted for USD to JPY;
-  int userinputUSDtoRMB; // User inputted for USD to RMB;
-  int userinputOunce; // User inputted for Ounce;
-  int userinputGram; // User inputted for Gram;
-  int fahrenheitToCelcius; // variable that stores the converted F->C;
-  int celciusToFahrenheit; // variable that stores the converted C->F;
-  float USDtoEURO ; // varaible that stores the converted USD->EURO;
-  float USDtoJPY; // stores the converted USD->JPY;
-  float USDtoRMB; // stores the converted USD->RMB;
-  float ounceToPounds; // stores the converted Ounce->Pounds;
-  float gramsToPounds; // stores the vonerted Grams->Pounds;
+  int userinputF; // User inputted Fahreinheit
+  int userinputC; // User inputted Celsius
+  int userinputUSDtoEuro; // User inputted for USD to EURO
+  int userinputUSDtoJPY; // User inputted for USD to JPY
+  int userinputUSDtoRMB; // User inputted for USD to RMB
+  int userinputOunce; // User inputted for Ounce
+  int userinputGram; // User inputted for Gram
+  int fahrenheitToCelcius; // variable that stores the converted F->C
+  int celciusToFahrenheit; // variable that stores the converted C->F
+  float USDtoEURO ; // varaible that stores the converted USD->EURO
+  float USDtoJPY; // stores the converted USD->JPY
+  float USDtoRMB; // stores the converted USD->RMB
+  float ounceToPounds; // stores the converted Ounce->Pounds
+  float gramsToPounds; // stores the vonerted Grams->Pounds
   
   printf("Welcome to Unit Converter! \n");
   printf("Here is a list of conversation to choose from: \n");
@@ -59,7 +59,7 @@ int main() {
           printf("Please enter the USD amount: \n");
           scanf("%d",&userinputUSDtoEuro);
           USDtoEURO = userinputUSDtoEuro * 0.87;
-          printf("Euro: %.2f",USDtoEURO); // %.2f = rounds the float to only 2 decimal places;
+          printf("Euro: %.2f",USDtoEURO); // %.2f = rounds the float to only 2 decimal places
       }
       else if(currencyChoice == 2){
           printf("Please enter the USD amount: \n");
